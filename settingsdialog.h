cd30f38dfd2467b244f9b17ab0ee7155f1df7475
cjaym <cjaym@yandex.ru>
2025-09-10 08:55:48 +0300

    Добавлено указание пути до git

--- settingsdialog.h
+++ settingsdialog.h
@@ -2,6 +2,7 @@
 #define SETTINGSDIALOG_H
 
 #include <QDialog>
+#include <QSettings>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,8 +18,18 @@ public:
     explicit SettingsDialog(QWidget *parent = nullptr);
     ~SettingsDialog();
 
+    QString getGitPath() const;
+    void setGitPath(const QString &gitPath);
+
+private slots:
+    void onGitPathButtonClicked();
+
 private:
+    void loadSettings();
+    void saveSettings();
+
     Ui::SettingsDialog *ui;
+    QSettings *settings;
 };
 
 #endif // SETTINGSDIALOG_H
