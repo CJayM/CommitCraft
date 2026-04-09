3bc677fe3bb86a7365e77bd536d6d1cdd53e41a4
cjaym <cjaym@yandex.ru>
2026-04-09 11:36:46 +0300

    Добавлен QTreeWidget для веток с корневыми узлами

--- commit_craft_app/brancheswidget.cpp
+++ commit_craft_app/brancheswidget.cpp
@@ -1,5 +1,6 @@
 #include "brancheswidget.h"
 #include <QTreeWidget>
+#include <QTreeWidgetItem>
 #include <QVBoxLayout>
 
 BranchesWidget::BranchesWidget(QWidget *parent)
@@ -12,6 +13,42 @@ BranchesWidget::BranchesWidget(QWidget *parent)
     m_treeWidget = new QTreeWidget(this);
     m_treeWidget->setHeaderHidden(true);
     m_treeWidget->setExpandsOnDoubleClick(true);
+    m_treeWidget->setIndentation(20);
+    m_treeWidget->setRootIsDecorated(true);
+    
+    setupTree();
     
     m_layout->addWidget(m_treeWidget);
+}
+
+void BranchesWidget::setupTree()
+{
+    // Устанавливаем количество колонок (1 для простоты)
+    m_treeWidget->setColumnCount(1);
+    
+    // Создаем корневые элементы
+    createRootItems();
+}
+
+void BranchesWidget::createRootItems()
+{
+    // Local Branches
+    m_localBranchesRoot = new QTreeWidgetItem(m_treeWidget);
+    m_localBranchesRoot->setText(0, "Local Branches");
+    m_localBranchesRoot->setExpanded(true); // Развернут по умолчанию
+    
+    // Remotes
+    m_remotesRoot = new QTreeWidgetItem(m_treeWidget);
+    m_remotesRoot->setText(0, "Remotes");
+    m_remotesRoot->setExpanded(false);
+    
+    // Tags
+    m_tagsRoot = new QTreeWidgetItem(m_treeWidget);
+    m_tagsRoot->setText(0, "Tags");
+    m_tagsRoot->setExpanded(false);
+    
+    // Stashes
+    m_stashesRoot = new QTreeWidgetItem(m_treeWidget);
+    m_stashesRoot->setText(0, "Stashes");
+    m_stashesRoot->setExpanded(false);
 }
--- commit_craft_app/brancheswidget.h
+++ commit_craft_app/brancheswidget.h
@@ -4,6 +4,7 @@
 #include <QFrame>
 
 class QTreeWidget;
+class QTreeWidgetItem;
 class QVBoxLayout;
 
 class BranchesWidget : public QFrame
@@ -14,8 +15,17 @@ public:
     explicit BranchesWidget(QWidget *parent = nullptr);
 
 private:
+    void setupTree();
+    void createRootItems();
+
     QVBoxLayout *m_layout;
     QTreeWidget *m_treeWidget;
+
+    // Корневые элементы дерева
+    QTreeWidgetItem *m_localBranchesRoot;
+    QTreeWidgetItem *m_remotesRoot;
+    QTreeWidgetItem *m_tagsRoot;
+    QTreeWidgetItem *m_stashesRoot;
 };
 
 #endif // BRANCHESWIDGET_H
