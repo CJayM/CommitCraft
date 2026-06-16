ae3713fda562fc36791564801422ecd6e24ce0ae
cjaym <cjaym@yandex.ru>
2026-06-16 21:04:04 +0300

    Подпрвлен размер SplashScreen

--- commit_craft_app/main.cpp
+++ commit_craft_app/main.cpp
@@ -3,11 +3,13 @@
 
 #include <QApplication>
 #include <QCoreApplication>
+#include <QGuiApplication>
 #include <QSplashScreen>
 #include <QTimer>
 #include <QPixmap>
 #include <QIcon>
 #include <QFont>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
@@ -25,19 +27,23 @@ int main(int argc, char *argv[])
 
     // Глобальная таблица стилей
     a.setStyleSheet(globalStylesheet());
-    
-    // Показываем splash screen
+
+    // Показываем splash screen (минимум 3 секунды)
     QPixmap splashPixmap(":/icons/icons/splash.png");
-    QSplashScreen splash(splashPixmap);
+    // Указываем DPR экрана — иначе Qt считает pixmap логическим и создаёт окно
+    // размером 800*DPI x 446*DPI физических пикселей, растягивая картинку
+    splashPixmap.setDevicePixelRatio(a.primaryScreen()->devicePixelRatio());
+    QSplashScreen splash(splashPixmap, Qt::WindowFlags(Qt::WindowType::WindowStaysOnTopHint));
     splash.show();
     a.processEvents();
-    
+
     MainWindow w;
     w.setWindowIcon(QIcon(":/icons/icons/icon.png"));
     w.show();
-    
-    // Скрываем splash через 3 секунды
-    QTimer::singleShot(3000, &splash, &QSplashScreen::close);
-    
+    a.processEvents();
+
+    // Скрываем splash через 3 секунды после появления главного окна
+    QTimer::singleShot(3000, &splash, &QSplashScreen::close);    
+
     return a.exec();
 }
